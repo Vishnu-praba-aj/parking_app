9874235ee200713defd83b8708e2c09c512da34b
Vishnu-praba-aj <vishnupraba893@gmail.com>
2025-06-12 15:21:04 +0530

    added advanced testcases

---
+++ advanced_tests/java_dynamic.java
@@ -0,0 +1,6 @@
+public class DynamicCar {
+    public void setField(String name, Object value) {
+        // Simulate dynamic field creation (reflection in real code)
+        // Not statically analyzable
+    }
+}
